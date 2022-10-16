1db360b665b53ae62ccc08c7e6c839742747dbe6
Student Alexandr <alexandr110282@yandex.ru>
2022-10-16 21:44:49 +0300

    Добавлены новые рабочие папки

---
+++ lesson64/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+ */
+
+//рекурсия последовательного вывода чисел в обратном порядке
+void PrintRange (int number)
+{
+    if (number==0)
+    {
+        return ;
+    }
+    System.Console.Write(number+", ");
+    PrintRange(number-1);
+}
+
+Console.WriteLine("Введите число N");
+int startUserNumber = Convert.ToInt32(Console.ReadLine());
+PrintRange(startUserNumber);
---
+++ lesson66/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+ */
+
+
+//функция ввода числовых данных
+int ReadInt (string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// // рекурсия нахождения суммы натуральных элементов в промежутке от M до N
+int SumNumber(int Number, int Degrees)
+{
+    
+    if (Degrees == Number)
+        {
+        return Number;
+    }
+    return Degrees + SumNumber(Number, --Degrees);
+    
+}
+
+int userNumber = ReadInt("Введите 1 число :");
+int userDegrees = ReadInt("Введите 2 число: ");
+int resultPow = SumNumber(userNumber,userDegrees);
+System.Console.WriteLine(resultPow);
